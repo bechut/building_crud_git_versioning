4be543161a06638196a80b11ee087816f504903e
Phuong Anh <anh.ngphuong@kyanon.digital>
2023-06-03 16:21:07 +0700

    features
    config antd, react-router
    install lodash

---
+++ src/routers/index.tsx
@@ -0,0 +1,86 @@
+import {
+  createBrowserRouter,
+  Link,
+  NavigateFunction,
+  Params,
+  RouterProvider,
+  useNavigate,
+  useParams,
+  useSearchParams,
+} from "react-router-dom";
+import metadata, { TRouterMetadata } from "./metadata";
+import { Result, Button } from "antd";
+import { memo, FC } from "react";
+// import { RootState } from "../redux";
+// import { useSelector } from "react-redux";
+// import cookie from "react-cookies";
+// import { TOKEN_NAME } from "../redux/api";
+
+export type LayoutProps = {
+  states: any;
+  params: Readonly<Params<string>>;
+  navigate: NavigateFunction;
+  accessToken: string;
+  searchParams: URLSearchParams;
+  setSearchParams: Function;
+  children?: React.FC
+};
+
+const TemplateComponent: FC<any> = ({ Layout, Page }) => {
+  const params = useParams();
+  const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  return (
+    <Layout
+      params={params}
+      navigate={navigate}
+      setSearchParams={setSearchParams}
+      searchParams={searchParams}
+    >
+      <Page />
+    </Layout>
+  );
+};
+
+const components: any = metadata
+  .map((m: TRouterMetadata) => {
+    let result = null;
+    try {
+      const Page = require(`../pages/${m.page}`).default;
+      const Layout = require(`../layout/${m.layout}`).default;
+      result = {
+        element: (
+          <TemplateComponent Layout={Layout} Page={Page} isAuth={m.auth} />
+        ),
+        path: m.path,
+      };
+    } catch (e) {}
+    return result;
+  })
+  .filter((x: any) => x);
+
+const router = createBrowserRouter([
+  ...components,
+  {
+    path: "*",
+    auth: false,
+    element: (
+      <Result
+        status="404"
+        title="404"
+        subTitle="Sorry, the page you visited does not exist."
+        extra={
+          <Link to="/">
+            <Button type="primary">Back Home</Button>
+          </Link>
+        }
+      />
+    ),
+  },
+]);
+
+const Routers = () => {
+  return <RouterProvider router={router} />;
+};
+export default memo(Routers);
